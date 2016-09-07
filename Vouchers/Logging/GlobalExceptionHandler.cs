b7b553cd657f0ebb67a50d3fe6737cfef44b3330
bimalsharma1 <bimalsharma1@outlook.com>
2016-09-07 19:31:20 +1000

    no message

---
+++ Vouchers/Logging/GlobalExceptionHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Results;
+
+//Author: Bimal Sharma
+//Description: Global Exception handler for the api
+namespace Dominos.OLO.Vouchers.Logging
+{
+    public class GlobalExceptionHandler : ExceptionHandler
+    {
+        public override void Handle(ExceptionHandlerContext context)
+        {
+            context.Result = new TextPlainErrorResult
+            {
+                Request = context.ExceptionContext.Request,
+                Content = "{ \"Message:\"Oops something wrong happened\"}"//pass some message as response
+            };
+        }
+
+        private class TextPlainErrorResult : IHttpActionResult
+        {
+            public HttpRequestMessage Request { get; set; }
+
+            public string Content { get; set; }
+
+            public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+            {
+                HttpResponseMessage response =
+                                 new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                response.Content = new StringContent(Content);
+                response.RequestMessage = Request;
+                return Task.FromResult(response);
+            }
+        }
+    }
+}
